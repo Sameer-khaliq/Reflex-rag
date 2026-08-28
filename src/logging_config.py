bd4e89ebee0d7a581969f8d286f7f3adfca801df
Sameer-khaliq <sameerkhaliq58@gmail.com>
2026-08-28 10:17:39 +0500

    Add logging configuration with a custom logger and bound logger class

---
+++ src/logging_config.py
@@ -0,0 +1,29 @@
+import logging
+
+
+def get_logger(trace_id: str = "default"):
+    logger = logging.getLogger(f"self_correcting_rag.{trace_id}")
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        handler.setFormatter(logging.Formatter("%(asctime)s %(name)s %(message)s"))
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+    return _BoundLogger(logger)
+
+
+class _BoundLogger:
+    def __init__(self, logger: logging.Logger):
+        self._logger = logger
+
+    def _format(self, event: str, **kwargs) -> str:
+        fields = " ".join(f"{k}={v}" for k, v in kwargs.items())
+        return f"{event} {fields}".strip()
+
+    def info(self, event: str, **kwargs) -> None:
+        self._logger.info(self._format(event, **kwargs))
+
+    def warning(self, event: str, **kwargs) -> None:
+        self._logger.warning(self._format(event, **kwargs))
+
+    def debug(self, event: str, **kwargs) -> None:
+        self._logger.debug(self._format(event, **kwargs))
